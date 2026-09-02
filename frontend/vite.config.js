d30a85b0efd163106f1eec05709da72ea67f0cba
Arshia <arshia4coding@gmail.com>
2026-09-03 00:57:16 +0330

    production

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,22 @@
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    proxy: {
-      // Forward API calls to the backend during development
-      '/api': 'http://127.0.0.1:8000',
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  const API_URL = env.VITE_API_URL || 'http://127.0.0.1:8000'
+
+  return {
+    plugins: [react(), tailwindcss()],
+
+    server: {
+      proxy: {
+        '/api': {
+          target: API_URL,
+          changeOrigin: true,
+        },
+      },
     },
-  },
+  }
 })
