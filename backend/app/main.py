44822438f648171f8d4e2e08e97c621b481f3f26
Arshia <arshia4coding@gmail.com>
2026-09-03 00:48:22 +0330

    production

--- backend/app/main.py
+++ backend/app/main.py
@@ -15,7 +15,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",
+        os.getenv("ALLOW_ORIGINS", "http://localhost:5173"),
     ],
     allow_credentials=True,
     allow_methods=["*"],
